ac2ce4865551da595a2f1e3dc1b65e94896cbd05
Parvinder Singh Gandhi <mr.parvindersinghgandhi@gmail.com>
2026-06-26 16:30:27 +0530

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import models
 from database import engine
 from api.customers import router as customer_router
@@ -8,14 +10,27 @@ from api.auth import router as auth_router
 
 app = FastAPI()
 
+# CORS Configuration
+origins = [
+    "http://localhost:5173",  
+    "https://mess-manager.vercel.app",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(customer_router)
 app.include_router(payment_router)
 app.include_router(meal_router)
 app.include_router(auth_router)
 
+
 @app.on_event("startup")
 def startup():
     models.Base.metadata.create_all(bind=engine)
-
-# db_dependency = Annotated[Session, Depends(get_db)]
-
